c2a9f1dde21e0a5ad53a1a1104e7d50aeb07b37c
Harikrishnan <haripdm9446718962@gmail.com>
2023-08-19 11:45:41 +0530

    i delete entire pgrm in hey file

--- hey.c
+++ hey.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-int main() {
-    int n, i;
-    unsigned long long fact = 1;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
 
-    // shows error if the user enters a negative integer
-    
-
-    return 0;
-}
